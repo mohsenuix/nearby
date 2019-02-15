537aa93651dfb72bf1bc0c218bbb4207163889d5
mohsenuix <mohsen0ix@gmail.com>
2019-02-15 09:36:24 +0330

    Add execution threads for usecases

---
+++ domain/src/main/java/ir/cafebazzar/app/domain/executor/UseCaseExecutor.kt
@@ -0,0 +1,13 @@
+package ir.cafebazzar.app.domain.executor
+
+import io.reactivex.Scheduler
+
+
+/**
+ * Represents an asynchronous execution for [interactor.ObservableUseCase].
+ * It's useful to execute use cases out of
+ * the UI thread to prevent it from freezing.
+ */
+interface UseCaseExecutor {
+    val scheduler: Scheduler
+}
